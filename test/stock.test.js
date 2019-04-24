f128a6a57bbe1790da1987bfd8259293929cd487
gregsoares <gregasoares@gmail.com>
2019-04-24 09:03:02 -0400

    indexCarousel size fixed

--- test/stock.test.js
+++ test/stock.test.js
@@ -6,6 +6,8 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 
+const app = require('../app');
+
 // Item Model
 const Item = require('../models/StockItem');
 
@@ -19,9 +21,38 @@ describe('/stock/getItem', () => {
   //   });
   // });
 
+  describe('GET /stock/allItems', () => {
+    it('should GET /allItems and return at least 1 Item', (done) => {
+      chai.request(app)
+        .get('/stock/allItems')
+        .end((err,res) => {
+          res.should.have.status(200);
+          // res.body.name.should.equal('ANAME');
+        done();
+        })
+        });
+    });
+  });
+
+describe('/stock/getItem', () => {
+  // Empty database before each test
+  // beforeEach((done) => {
+  //   Item.remove({}, (err) => {
+  //   done(); 
+  //   });
+  // });
+
   describe('GET /stock/getItem - name = ANAME', () => {
-    it('should GET ', () => {
-      
+    it('should GET item name: "ANAMEs"', () => {
+      chai.request(app)
+        .get('/stock/getItem', (req,res) => {
+          name: 'ANAMEs'
+        })
+        .end((err,res) => {
+          res.should.have.status(200);
+          // res.body.name.should.equal('ANAME');
+        done();
+        });
     });
   });
 });
